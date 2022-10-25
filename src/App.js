75afc8f1cee69efdbf075542450f1222d38d1ff2
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-25 14:31:34 +0300

    style: create chat-container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-[##ffffff] mt-10 shadow-xl border rounded-t-3xl relative`,
+};
+
 function App() {
-  return <div className="bg-blue-400 p-4 text-center">Chat app</div>;
+  return (
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}></section>
+    </div>
+  );
 }
 
 export default App;
