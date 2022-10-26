fc98e247531c2363208d2b1b9d526772c785b31e
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-26 23:26:59 +0300

    feat: add custom styles for sent and received messages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
         <Navbar />
-        <Chat />
+        {user ? <Chat /> : null}
       </section>
     </div>
   );
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { auth } from '../firebase';
 
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
   name: `absolute mt-[-4rem] text-gray-600 text-xs`,
+  sent: `bg-[#c8f246] text-black flex-row-reverse text-end float-right rounded-bl-full`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
 };
 
 const Message = ({ message }) => {
+  const messageClass = message.uid === auth.currentUser.uid ? `${style.sent}` : `${style.received}`;
   return (
     <div>
-      <div className={style.message}>
+      <div className={`${style.message} ${messageClass}`}>
         <p className={style.name}>{message.name}</p>
         <p>{message.text}</p>
       </div>
